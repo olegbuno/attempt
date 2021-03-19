c798883b7fc82894c89854defdfa4c88f724d331
olegbuno <72154953+olegbuno@users.noreply.github.com>
2021-03-19 22:37:57 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 define('CREATED',2013);
 define('SKIN','default');
-define('DB_NAME','main');
-define('DB_LOGIN','root');
-define('DB_PASS','');
-define('DB_LOCAL','localhost');
+define('DB_NAME','vq97uc3m9tks9wxa');
+define('DB_LOGIN','olvo5ml0jiiu4efe');
+define('DB_PASS','w7r55d2737aio9vs');
+define('DB_LOCAL','xlf3ljx3beaucz9x.cbetxkdyhwsb.us-east-1.rds.amazonaws.com');
